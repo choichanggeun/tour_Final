e3307ac0b562e347e32dff8a0a1660375a0b3960
choichanggeun <choichanggeoun@gmail.com>
2023-08-18 10:27:03 +0900

    feat 여행계획 수정

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -2,13 +2,24 @@ const TourService = require('../services/diary.service');
 
 class TourController {
   tourService = new TourService();
-
-  postTour = async (req, res) => {
+  //여행 계획 등록
+  postTour = async (req, res, next) => {
     try {
-      return res.status(200).json({ message: '메세지 내용' });
-    } catch (error) {
-      console.log(error.stack);
-      return res.status(error.status || 500).send({ message: `${error.message}` });
+      const { user_id, nickname } = req.locals.user;
+      const { title, start_date, end_date } = req.body;
+      const { code, message } = await this.tourService.postTour({
+        user_id,
+        nickname,
+        title,
+        content,
+        start_date,
+        end_date,
+      });
+      return res.status(code).json({ message });
+    } catch (err) {
+      if (err.code) return res.status(err.code).json({ message: err.message });
+      console.log(err);
+      return res.status.send({ message: err.message });
     }
   };
   getTour = async (req, res) => {
--- services/tour.service.js
+++ services/tour.service.js
@@ -3,10 +3,23 @@ const TourRepository = require('../repositories/tour.repository');
 class TourService {
   tourRepository = new TourRepository();
 
-  postTour = async () => {
-    if (true) {
-      throw new CustomError(404, '에러 메세지 내용.');
-    }
+  postTour = async ({ user_id, nickname, title, content, start_date, end_date }) => {
+    if (!user_id) throw { code: 401, message: 'userId가 존재하지 않습니다.' };
+
+    if (!cardId) throw { code: 401, message: 'cardId가 존재하지 않습니다.' };
+
+    if (!content) throw { code: 401, message: '댓글 내용을 입력해주세요.' };
+
+    const createCommentData = await this.commentsRepository.createComments({
+      cardId,
+      content,
+      createUser: userId,
+      name,
+    });
+
+    if (!createCommentData) throw { code: 401, message: '댓글 등록이 실패하였습니다. 않습니다.' };
+
+    return { createCommentData, code: 200, message: '게시글 작성이 완료되었습니다.' };
   };
 
   getTour = async () => {
