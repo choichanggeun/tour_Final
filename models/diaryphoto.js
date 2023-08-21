f1a097211b55e826b64ea52d8914b00fe954539f
wonbin choi <fnffnqls@naver.com>
2023-08-21 20:00:55 +0900

    fix: diaryphoto 모델 수정

--- models/diaryphoto.js
+++ models/diaryphoto.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.Diaries, {
+      this.belongsTo(models.Diary, {
         targetKey: 'id',
         foreignKey: 'diary_id',
       });
@@ -27,7 +27,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         type: DataTypes.INTEGER,
         references: {
-          model: 'Diaries',
+          model: 'Diary',
           key: 'id',
         },
       },
