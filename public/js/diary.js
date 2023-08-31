fb141a07f9e0baf2881f5a8897a3ec651b433d23
wonbin choi <fnffnqls@naver.com>
2023-08-31 13:02:54 +0900

    fix: diary 수정 중

--- public/js/diary.js
+++ public/js/diary.js
@@ -69,9 +69,10 @@ const updateDiary = function () {
   const currentPageURL = window.location.href;
   const urlParams = new URLSearchParams(window.location.search);
   const diary_id = urlParams.get('diary_id');
-  const targetPageURL = 'http://localhost:3000/diary-post.html?';
+  const targetPageURL = `http://localhost:3000/diary-update.html?diary_id=${diary_id}`;
   if (currentPageURL === targetPageURL) {
-    document.getElementById('diary-post-button').addEventListener('click', async () => {
+    document.getElementById('diary-img-delete-btn').addEventListener('click', async () => {});
+    document.getElementById('diary-update-button').addEventListener('click', async () => {
       const diaryTitle = document.getElementById('diary-title').value;
       const diaryContent = document.getElementById('diary-content').value;
 
@@ -160,22 +161,66 @@ const getDiary = function () {
               <p>내용: ${data.content}</p>
             </div>
             `;
-          for (let image of images) {
+          if (images) {
+            for (let image of images) {
+              const diaryBox = document.getElementById(`diaryBox${data.id}`);
+              const img = document.createElement('img');
+              img.src = `https://final-tour-2.s3.ap-northeast-2.amazonaws.com/diary-img/${image.diary_img}`;
+              diaryBox.appendChild(img);
+            }
+          } else {
             const diaryBox = document.getElementById(`diaryBox${data.id}`);
             const img = document.createElement('img');
-            img.src = `https://final-tour-2.s3.ap-northeast-2.amazonaws.com/diary-img/${image.diary_img}`;
+            img.src = `https://final-tour-2.s3.ap-northeast-2.amazonaws.com/etc/no_img.png`;
             diaryBox.appendChild(img);
           }
+          row.innerHTML += `
+            <button><a href="http://localhost:3000/diary-update.html?diary_id=${diary_id}" id="diary-update-btn">수정</a></button>
+            <button id="diary-delete-btn">삭제</button>
+          `;
+          document.getElementById('diary-delete-btn').addEventListener('click', async function () {
+            deleteDiary(diary_id);
+          });
         } else {
           alert(data.message);
         }
-      } catch (err) {
-        console.error('Error:', err);
+      } catch (error) {
+        console.error('Error:', error);
       }
     });
   }
 };
 getDiary();
+
+// 여행 일지, 이미지 삭제
+const deleteDiary = async function (diary_id) {
+  try {
+    // 이미지 모두 삭제 후 여행 일지 삭제 가능
+    // 삭제할 여행 일지 이미지 목록 가져오기
+    const response = await fetch(`/diaries/${diary_id}/photos`);
+    const { images } = await response.json();
+    // 이미지 있으면 모두 삭제
+    if (images) {
+      for (let image of images) {
+        await fetch(`/photos/${image.id}`, {
+          method: 'DELETE',
+        });
+      }
+    }
+    const response2 = await fetch(`/diaries/${diary_id}`, {
+      method: 'DELETE',
+    });
+    const { data } = await response2.json();
+    if (response2.ok) {
+      alert('여행 일지를 삭제했습니다.');
+      location.href = '/';
+    } else {
+      alert(data.message);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
 
 // 모든 여행 일지, 이미지 조회
 const getAllDiary = function () {
@@ -194,8 +239,7 @@ const getAllDiary = function () {
         const { data } = await response.json();
         const { images } = await response2.json();
         if (response.ok) {
-          const row = document.getElementById('row');
-          let count = 4;
+          const cardList = document.getElementById('cardList');
           for (let diary of data) {
             let diaryImg;
             for (let image of images) {
@@ -204,55 +248,33 @@ const getAllDiary = function () {
                 break;
               }
             }
-            if (count % 4 === 0) {
-              row.innerHTML += `
-              <div class="row-md-3" id="cardList${~~(count / 4)}">
-                <div class="card" id="card${count - 3}">
-                  <a href="http://localhost:3000/diary-detail.html?diary_id=${diary.id}"><img class="img-fluid" src="https://final-tour-2.s3.ap-northeast-2.amazonaws.com/diary-img/${diaryImg.diary_img}" alt="" /></a>
-                  <div class="card-block">
-                    <div class="news-title">
-                      <h2 class="title-small">${diary.title}</h2>
-                    </div>
+            cardList.innerHTML += `
+              <div class="diary-card" id="diary-card">
+                <div class="diary-img-box" id="diaryImgBox${diary.id}"></div>
+                <div class="card-block">
+                  <div class="diary-title">
+                    <h2 class="title-small">${diary.title}</h2>
                   </div>
                 </div>
               </div>
               `;
+            if (diaryImg) {
+              const diaryImgBox = document.getElementById(`diaryImgBox${diary.id}`);
+              diaryImgBox.innerHTML += `
+              <a href="http://localhost:3000/diary-detail.html?diary_id=${diary.id}"><img class="diary-img" src="https://final-tour-2.s3.ap-northeast-2.amazonaws.com/diary-img/${diaryImg.diary_img}" alt=""/></a>
+              `;
             } else {
-              const cardList = document.getElementById(`cardList${~~(count / 4)}`);
-              cardList.innerHTML += `
-              <div class="card" id="card${count - 3}">
-                <a href="http://localhost:3000/diary-detail.html?diary_id=${diary.id}"><img class="img-fluid" src="https://final-tour-2.s3.ap-northeast-2.amazonaws.com/diary-img/${diaryImg.diary_img}" alt="" /></a>
-                  <div class="card-block">
-                    <div class="news-title">
-                      <h2 class="title-small">${diary.title}</h2>
-                    </div>
-                  </div>
-                </div>
+              const diaryImgBox = document.getElementById(`diaryImgBox${diary.id}`);
+              diaryImgBox.innerHTML += `
+              <a href="http://localhost:3000/diary-detail.html?diary_id=${diary.id}"><img class="diary-img" src="https://final-tour-2.s3.ap-northeast-2.amazonaws.com/etc/no_img.png" alt=""/></a>
               `;
             }
-            count++;
-          }
-
-          while (count % 4 !== 0) {
-            console.log(count);
-            const cardList = document.getElementById(`cardList${~~(count / 4)}`);
-            cardList.innerHTML += `
-              <div class="card" id="card${count - 3}" style="visibility: hidden;">
-                  <img class="img-fluid" src="" alt="" />
-                  <div class="card-block">
-                    <div class="news-title">
-                      <h2 class="title-small"></h2>
-                    </div>
-                  </div>
-                </div>
-              `;
-            count++;
           }
         } else {
           alert(data.message);
         }
-      } catch (err) {
-        console.error('Error:', err);
+      } catch (error) {
+        console.error('Error:', error);
       }
     });
   }
@@ -286,20 +308,27 @@ const getMyDiary = function () {
                   <p>내용: ${diary.content}</p>
                   </div>
                   `;
-            for (let image of images) {
-              if (diary.id === image.diary_id) {
-                const diaryBox = document.getElementById(`diaryBox${diary.id}`);
-                const img = document.createElement('img');
-                img.src = `https://final-tour-2.s3.ap-northeast-2.amazonaws.com/diary-img/${image.diary_img}`;
-                diaryBox.appendChild(img);
+            if (images) {
+              for (let image of images) {
+                if (diary.id === image.diary_id) {
+                  const diaryBox = document.getElementById(`diaryBox${diary.id}`);
+                  const img = document.createElement('img');
+                  img.src = `https://final-tour-2.s3.ap-northeast-2.amazonaws.com/diary-img/${image.diary_img}`;
+                  diaryBox.appendChild(img);
+                }
               }
+            } else {
+              const diaryImgBox = document.getElementById(`diaryImgBox${diary.id}`);
+              diaryImgBox.innerHTML += `
+              <a href="http://localhost:3000/diary-detail.html?diary_id=${diary.id}"><img class="diary-img" src="https://final-tour-2.s3.ap-northeast-2.amazonaws.com/etc/no_img.png" alt=""/></a>
+              `;
             }
           }
         } else {
           alert(data.message);
         }
-      } catch (err) {
-        console.error('Error:', err);
+      } catch (error) {
+        console.error('Error:', error);
       }
     });
   }
@@ -333,20 +362,27 @@ const getTourDiary = function () {
                 <p>내용: ${diary.content}</p>
               </div>
               `;
-            for (let image of images) {
-              if (diary.id === image.diary_id) {
-                const diaryBox = document.getElementById(`diaryBox${diary.id}`);
-                const img = document.createElement('img');
-                img.src = `https://final-tour-2.s3.ap-northeast-2.amazonaws.com/diary-img/${image.diary_img}`;
-                diaryBox.appendChild(img);
+            if (images) {
+              for (let image of images) {
+                if (diary.id === image.diary_id) {
+                  const diaryBox = document.getElementById(`diaryBox${diary.id}`);
+                  const img = document.createElement('img');
+                  img.src = `https://final-tour-2.s3.ap-northeast-2.amazonaws.com/diary-img/${image.diary_img}`;
+                  diaryBox.appendChild(img);
+                }
               }
+            } else {
+              const diaryImgBox = document.getElementById(`diaryImgBox${diary.id}`);
+              diaryImgBox.innerHTML += `
+              <a href="http://localhost:3000/diary-detail.html?diary_id=${diary.id}"><img class="diary-img" src="https://final-tour-2.s3.ap-northeast-2.amazonaws.com/etc/no_img.png" alt=""/></a>
+              `;
             }
           }
         } else {
           alert(data.message);
         }
-      } catch (err) {
-        console.error('Error:', err);
+      } catch (error) {
+        console.error('Error:', error);
       }
     });
   }
