2f4d5d6474d7934c0325ef075fd39d7a876881c9
dldPwls03 <lyj3494@naver.com>
2023-09-11 20:47:02 +0900

    프론트

--- public/js/diary-detail.js
+++ public/js/diary-detail.js
@@ -6,57 +6,96 @@ const getDiary = function () {
   const diary_id = urlParams.get('diary_id');
   const targetPageURL = `http://localhost:3000/diary-detail.html?diary_id=${diary_id}`;
   if (currentPageURL === targetPageURL) {
-    addEventListener('DOMContentLoaded', async function renderDiary() {
-      const row = document.getElementById('row');
-      try {
-        const response = await fetch(`/diaries/${Number(diary_id)}`, {
-          method: 'GET',
-        });
-        const response2 = await fetch(`/diaries/${Number(diary_id)}/photos`, {
-          method: 'GET',
-        });
-        const { data } = await response.json();
-        const { images } = await response2.json();
-        if (response.ok) {
-          if (images.length !== 0) {
-            row.innerHTML += `
-                <div id="diaryBox${data.id}">
-                  <h5 style="padding: 1px; margin: 0 500px;">제목: ${data.title}</h5>
-                </div>
-                `;
-            for (let image of images) {
-              const diaryBox = document.getElementById(`diaryBox${data.id}`);
-              diaryBox.innerHTML += `
-                <img src="https://final-tour-2.s3.ap-northeast-2.amazonaws.com/diary-img/${image.diary_img}">
-                `;
-            }
-          } else {
-            row.innerHTML += `
-                <div id="diaryBox${data.id}">
-                <h5 style="padding: 1px; margin: 0 500px;">제목: ${data.title}</h5>
-                  <img src="https://final-tour-2.s3.ap-northeast-2.amazonaws.com/etc/no_img.png">
-                </div>
-              `;
-          }
-          const diaryBox = document.getElementById(`diaryBox${data.id}`);
-          diaryBox.innerHTML += `
-            <p>내용: ${data.content}</p>
-            <button><a href="http://localhost:3000/diary-update.html?diary_id=${diary_id}">일지 수정</a></button>
-            <button id="diary-delete-btn">일지 삭제</button>
-            `;
-          document.getElementById('diary-delete-btn').addEventListener('click', async function () {
-            deleteDiary(diary_id);
-          });
-        } else {
-          alert(data.message);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    });
+    renderDiary(diary_id);
   }
 };
-getDiary();
+
+// Function to render diary content
+const renderDiary = async (diary_id) => {
+  try {
+    const response = await fetch(`/diaries/${Number(diary_id)}`, {
+      method: 'GET',
+    });
+    const response2 = await fetch(`/diaries/${Number(diary_id)}/photos`, {
+      method: 'GET',
+    });
+    const { data } = await response.json();
+    const { images } = await response2.json();
+    if (response.ok) {
+      const row = document.getElementById('row');
+
+      // Create a card element
+      const diaryCard = document.createElement('div');
+      diaryCard.classList.add('card');
+
+      // Create a card body
+      const cardBody = document.createElement('div');
+      cardBody.classList.add('card-body');
+
+      // Diary title
+      const title = document.createElement('h5');
+      title.classList.add('card-title');
+      title.textContent = `제목: ${data.title}`;
+
+      // Append the title to the card body
+      cardBody.appendChild(title);
+
+      // Check if there are images to display
+      if (images.length !== 0) {
+        for (let image of images) {
+          const diaryCardImage = document.createElement('img');
+          diaryCardImage.classList.add('card-img-top');
+          diaryCardImage.src = `https://final-tour-2.s3.ap-northeast-2.amazonaws.com/diary-img/${image.diary_img}`;
+          diaryCardImage.alt = 'Diary Image';
+          diaryCard.appendChild(diaryCardImage);
+        }
+      } else {
+        // Display a default image if there are no images
+        const defaultImage = document.createElement('img');
+        defaultImage.classList.add('card-img-top');
+        defaultImage.src = 'https://final-tour-2.s3.ap-northeast-2.amazonaws.com/etc/no_img.png';
+        defaultImage.alt = 'No Image Available';
+        diaryCard.appendChild(defaultImage);
+      }
+
+      // Diary content
+      const content = document.createElement('p');
+      content.classList.add('card-text');
+      content.textContent = `내용: ${data.content}`;
+
+      // Append the content to the card body
+      cardBody.appendChild(content);
+
+      // Edit button
+      const editButton = document.createElement('button');
+      editButton.classList.add('btn', 'btn-primary', 'mt-3'); // btn-primary 클래스 추가
+      editButton.innerHTML = `<a href="http://localhost:3000/diary-update.html?diary_id=${diary_id}">일지 수정</a>`;
+      // Delete button
+      const deleteButton = document.createElement('button');
+      deleteButton.classList.add('btn', 'btn-danger', 'mt-3'); // btn-danger 클래스 추가
+      deleteButton.textContent = '일지 삭제';
+      deleteButton.addEventListener('click', async () => {
+        deleteDiary(diary_id);
+      });
+
+      // Append the buttons to the card body
+      cardBody.appendChild(editButton);
+      cardBody.appendChild(deleteButton);
+
+      // Append the card body to the card
+      diaryCard.appendChild(cardBody);
+
+      // Append the card to the row
+      row.appendChild(diaryCard);
+    } else {
+      alert(data.message);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+// Call getDiary when the DOM is loaded
+document.addEventListener('DOMContentLoaded', getDiary);
 
 // 여행 일지, 이미지 삭제
 const deleteDiary = async function (diary_id) {
--- public/js/diary-update.js
+++ public/js/diary-update.js
@@ -1,117 +1,124 @@
-// 여행 일지 수정 페이지
+// 여행 일지 수정 페이지 렌더링
 const updateRenderDiary = function () {
   const currentPageURL = window.location.href;
   const urlParams = new URLSearchParams(window.location.search);
   const diary_id = urlParams.get('diary_id');
   const targetPageURL = `http://localhost:3000/diary-update.html?diary_id=${diary_id}`;
+
   if (currentPageURL === targetPageURL) {
-    addEventListener('DOMContentLoaded', async function renderDiary() {
-      const row = document.getElementById('row');
+      addEventListener('DOMContentLoaded', async function () {
+          const row = document.getElementById('row');
+          const imageListContainer = document.getElementById('image-list');
 
-      try {
-        const response = await fetch(`/diaries/${Number(diary_id)}`, {
-          method: 'GET',
-        });
-        const response2 = await fetch(`/diaries/${Number(diary_id)}/photos`, {
-          method: 'GET',
-        });
-        const { data } = await response.json();
-        const { images } = await response2.json();
-        if (response.ok) {
-          row.innerHTML += `
-            <div id='diaryBox${data.id}'>
-              <label for="title">제목: </label>
-              <input name="title" value="${data.title}" id="diary-title"></input>
-            </div>
-            `;
-          if (images.length !== 0) {
-            for (let image of images) {
-              const diaryBox = document.getElementById(`diaryBox${data.id}`);
-              diaryBox.innerHTML += `
-                <img src="https://final-tour-2.s3.ap-northeast-2.amazonaws.com/diary-img/${image.diary_img}" id="img${image.id}">
-                <button id="img-delete-btn${image.id}">X</button>
-              `;
-            }
-          } else {
-            const diaryBox = document.getElementById(`diaryBox${data.id}`);
-            diaryBox.innerHTML += `
-                <img src="https://final-tour-2.s3.ap-northeast-2.amazonaws.com/etc/no_img.png">
-              `;
-          }
-          const isChecked = data.status === '1' ? 'checked' : '';
-          const diaryBox = document.getElementById(`diaryBox${data.id}`);
-          diaryBox.innerHTML += `
-            <label for="content">내용: </label>
-            <textarea name="content" id="diary-content">${data.content}</textarea>
-            <input type="file" id="diary-image" multiple />
-            <label for="private">비공개: </label>
-            <input type="checkbox" name="private" id="private-checkbox" ${isChecked}/>
-            <button id='update-cancel-btn' >취소</button>
-            <button id='diary-update-btn'>저장</button>
-          `;
-          const updateCancelBtn = document.getElementById('update-cancel-btn');
-          if (updateCancelBtn) {
-            updateCancelBtn.addEventListener('click', async function () {
-              location.href = `http://localhost:3000/diary-detail.html?diary_id=${diary_id}`;
-            });
-          }
+          try {
+              const response = await fetch(`/diaries/${Number(diary_id)}`, {
+                  method: 'GET',
+              });
+              const response2 = await fetch(`/diaries/${Number(diary_id)}/photos`, {
+                  method: 'GET',
+              });
+              const { data } = await response.json();
+              const { images } = await response2.json();
 
-          document.addEventListener('click', async function (e) {
-            let imgId;
-            const img = e.target.previousElementSibling;
-            if (img && img.tagName === 'IMG') {
-              imgId = img.id;
+              if (response.ok) {
+                  row.innerHTML += `
+                      <div id='diaryBox${data.id}' class='updated-card'>
+                          <label for="title">제목</label>
+                          <input name="title" value="${data.title}" id="diary-title" class="form-control">
+                      </div>
+                  `;
 
-              const photo_id = Number([...imgId].filter((n) => !isNaN(n) === true).join(''));
-              deleteDiaryImg(photo_id);
-            }
-          });
-          const diaryUpdateBtn = document.getElementById('diary-update-btn');
-          if (diaryUpdateBtn) {
-            diaryUpdateBtn.addEventListener('click', async function () {
-              updateDiary(diary_id);
-              updateImg();
-            });
+                  if (images.length !== 0) {
+                      for (let image of images) {
+                          const diaryBox = document.getElementById(`diaryBox${data.id}`);
+                          diaryBox.innerHTML += `
+                              <div class="image-container">
+                                  <img src="https://final-tour-2.s3.ap-northeast-2.amazonaws.com/diary-img/${image.diary_img}" id="img${image.id}" class="diary-image">
+                                  <button id="img-delete-btn${image.id}" class="btn btn-danger">X</button>
+                              </div>
+                          `;
+                      }
+                  } else {
+                      const diaryBox = document.getElementById(`diaryBox${data.id}`);
+                      diaryBox.innerHTML += `
+                          <img src="https://final-tour-2.s3.ap-northeast-2.amazonaws.com/etc/no_img.png" class="diary-image">
+                      `;
+                  }
+
+                  const isChecked = data.status === '1' ? 'checked' : '';
+                  const diaryBox = document.getElementById(`diaryBox${data.id}`);
+                  diaryBox.innerHTML += `
+                      <label for="content"></label>
+                      <textarea name="content" id="diary-content" class="form-control">${data.content}</textarea>
+                      <label for="private">비공개</label>
+                      <input type="checkbox" name="private" id="private-checkbox" ${isChecked} class="form-check-input">
+                      <input type="file" id="diary-image-upload" multiple class="form-control">
+                      <button id='update-cancel-btn' class="btn btn-secondary">취소</button>
+                      <button id='diary-update-btn' class="btn btn-primary">저장</button>
+                  `;
+
+                  const updateCancelBtn = document.getElementById('update-cancel-btn');
+                  if (updateCancelBtn) {
+                      updateCancelBtn.addEventListener('click', async function () {
+                          location.href = `http://localhost:3000/diary-detail.html?diary_id=${diary_id}`;
+                      });
+                  }
+
+                  document.addEventListener('click', async function (e) {
+                      if (e.target.matches('.btn-danger')) {
+                          const imgId = e.target.previousElementSibling.id;
+                          const photo_id = Number([...imgId].filter((n) => !isNaN(n) === true).join(''));
+                          deleteDiaryImg(photo_id);
+                      }
+                  });
+
+                  const diaryUpdateBtn = document.getElementById('diary-update-btn');
+                  if (diaryUpdateBtn) {
+                      diaryUpdateBtn.addEventListener('click', async function () {
+                          updateDiary(diary_id);
+                          updateImg();
+                      });
+                  }
+              } else {
+                  alert('여행일지 수정에 실패했습니다.');
+              }
+          } catch (error) {
+              alert('여행일지 수정에 실패했습니다.');
+              console.error(error);
           }
-        } else {
-          alert('여행일지 수정에 실패했습니다.');
-        }
-      } catch (error) {
-        alert('여행일지 수정에 실패했습니다.');
-        console.error(error);
-      }
-    });
+      });
   }
 };
+
 updateRenderDiary();
 
 // 여행 일지 수정
-updateDiary = async function (diary_id) {
+const updateDiary = async function (diary_id) {
   const diaryTitle = document.getElementById('diary-title').value;
   const diaryContent = document.getElementById('diary-content').value;
   const isPrivate = document.getElementById('private-checkbox').checked;
   const status = isPrivate ? 1 : 0;
 
   try {
-    const response = await fetch(`/diaries/${diary_id}`, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        title: diaryTitle,
-        content: diaryContent,
-        status: status,
-      }),
-    });
-    const data = await response.json();
-    if (response.ok) {
-      alert(data.message);
-      location.href = `http://localhost:3000/diary-detail.html?diary_id=${diary_id}`;
-    } else {
-      alert(data.message);
-    }
+      const response = await fetch(`/diaries/${diary_id}`, {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+              title: diaryTitle,
+              content: diaryContent,
+              status: status,
+          }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+          alert(data.message);
+          location.href = `http://localhost:3000/diary-detail.html?diary_id=${diary_id}`;
+      } else {
+          alert(data.message);
+      }
   } catch (error) {
-    alert('여행 일지 수정에 실패했습니다.');
-    console.error(error);
+      alert('여행 일지 수정에 실패했습니다.');
+      console.error(error);
   }
 };
 
@@ -119,42 +126,42 @@ updateDiary = async function (diary_id) {
 const updateImg = async function () {
   const urlParams = new URLSearchParams(window.location.search);
   const diary_id = urlParams.get('diary_id');
-  const uploadImg = document.getElementById('diary-image').files;
+  const uploadImg = document.getElementById('diary-image-upload').files;
   const formData = new FormData();
   if (uploadImg.length !== 0) {
-    // 여러 이미지를 하나씩 formData에 할당
-    for (let i = 0; i < uploadImg.length; i++) {
-      formData.append('image', uploadImg[i]);
-    }
+      // 여러 이미지를 하나씩 formData에 할당
+      for (let i = 0; i < uploadImg.length; i++) {
+          formData.append('image', uploadImg[i]);
+      }
   }
   try {
-    await fetch(`/diaries/${diary_id}/photos`, {
-      method: 'POST',
-      body: formData,
-    });
+      await fetch(`/diaries/${diary_id}/photos`, {
+          method: 'POST',
+          body: formData,
+      });
   } catch (error) {
-    alert('이미지 업로드를 실패했습니다.');
-    console.error(error);
+      alert('이미지 업로드를 실패했습니다.');
+      console.error(error);
   }
 };
 
 // 여행 일지 이미지 삭제
 const deleteDiaryImg = async function (photo_id) {
   try {
-    const response = await fetch(`/photos/${photo_id}`, {
-      method: 'DELETE',
-    });
-    const data = await response.json();
-    if (response.ok) {
-      const img = document.getElementById(`img${photo_id}`);
-      img.style.display = 'none';
-      const imgDeleteBtn = document.getElementById(`img-delete-btn${photo_id}`);
-      imgDeleteBtn.style.display = 'none';
-      alert(data.message);
-    } else {
-      alert(data.message);
-    }
+      const response = await fetch(`/photos/${photo_id}`, {
+          method: 'DELETE',
+      });
+      const data = await response.json();
+      if (response.ok) {
+          const img = document.getElementById(`img${photo_id}`);
+          img.style.display = 'none';
+          const imgDeleteBtn = document.getElementById(`img-delete-btn${photo_id}`);
+          imgDeleteBtn.style.display = 'none';
+          alert(data.message);
+      } else {
+          alert(data.message);
+      }
   } catch (error) {
-    console.error(error);
+      console.error(error);
   }
 };
