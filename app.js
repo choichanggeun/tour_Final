d81e29825113e97388902037d1ad7cf4d098c495
Lee <jrmun123@gmail.com>
2023-09-12 09:20:02 +0900

    test: test

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const fs = require('fs');
 const app = express();
 const http = require('http').createServer(app);
 
-var redis = require('redis');
-var redisAdapter = require('socket.io-redis');
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
