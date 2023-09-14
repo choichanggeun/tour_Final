e3bd189a2c1c276dfac0dba8638ac2806a1cfaea
choichanggeun <choichanggeoun@gmail.com>
2023-09-14 18:19:12 +0900

    tour-detail 로고 및 드롭박스 수정완료

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,6 @@
 // 사용자 정보 확인하여 로그인 상태에 따라 버튼 표시
+let flag = false;
+
 function checkLoggedInStatus() {
   fetch('/users/me', {
     method: 'GET',
@@ -14,16 +16,35 @@ function checkLoggedInStatus() {
         // 이미지 클릭 이벤트 추가
         var profilePic = document.getElementById('profilePic');
         var dropdownMenu = document.getElementById('dropdownMenu');
-
+        console.log('1');
         profilePic.addEventListener('click', function (event) {
           event.stopPropagation(); // Stop the event from bubbling up to the window object
           if (dropdownMenu.style.display === 'block') {
             dropdownMenu.style.display = 'none';
+            console.log('success');
           } else {
             dropdownMenu.style.display = 'block';
+            console.log('fail');
           }
         });
+        console.log('2');
+        // var profilePic2 = document.getElementById('profilePic2');
+        // var dropdownMenu2 = document.getElementById('dropdownMenu2');
 
+        // profilePic2.addEventListener('click', function (event) {
+        //   event.stopPropagation(); // Stop the event from bubbling up to the window object
+        //   if (dropdownMenu2.style.display === 'block') {
+        //     dropdownMenu2.style.display = 'none';
+        //     console.log('success');
+        //     console.log('!!!');
+        //   } else {
+        //     dropdownMenu2.style.display = 'block';
+        //     console.log('fail');
+        //     console.log('???');
+        //   }
+        // });
+
+        console.log('3');
         // Window click event to hide the dropdown menu when clicked outside
         window.addEventListener('click', function (event) {
           if (dropdownMenu.style.display === 'block') {
@@ -31,6 +52,7 @@ function checkLoggedInStatus() {
           }
         });
 
+        console.log('4');
         document.querySelector('#loginbtn').style.display = 'none';
       } else {
         document.querySelector('#loginbtn').style.display = 'block';
