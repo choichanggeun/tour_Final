b0895d40431ea025be69e9852834171500a8795e
choichanggeun <choichanggeoun@gmail.com>
2023-09-14 20:03:04 +0900

    tour-detail 드롭박스 수정완료

--- public/js/common.js
+++ public/js/common.js
@@ -16,7 +16,7 @@ function checkLoggedInStatus() {
         // 이미지 클릭 이벤트 추가
         var profilePic = document.getElementById('profilePic');
         var dropdownMenu = document.getElementById('dropdownMenu');
-        console.log('1');
+
         profilePic.addEventListener('click', function (event) {
           event.stopPropagation(); // Stop the event from bubbling up to the window object
           if (dropdownMenu.style.display === 'block') {
@@ -27,24 +27,7 @@ function checkLoggedInStatus() {
             console.log('fail');
           }
         });
-        console.log('2');
-        // var profilePic2 = document.getElementById('profilePic2');
-        // var dropdownMenu2 = document.getElementById('dropdownMenu2');
 
-        // profilePic2.addEventListener('click', function (event) {
-        //   event.stopPropagation(); // Stop the event from bubbling up to the window object
-        //   if (dropdownMenu2.style.display === 'block') {
-        //     dropdownMenu2.style.display = 'none';
-        //     console.log('success');
-        //     console.log('!!!');
-        //   } else {
-        //     dropdownMenu2.style.display = 'block';
-        //     console.log('fail');
-        //     console.log('???');
-        //   }
-        // });
-
-        console.log('3');
         // Window click event to hide the dropdown menu when clicked outside
         window.addEventListener('click', function (event) {
           if (dropdownMenu.style.display === 'block') {
@@ -52,7 +35,6 @@ function checkLoggedInStatus() {
           }
         });
 
-        console.log('4');
         document.querySelector('#loginbtn').style.display = 'none';
       } else {
         document.querySelector('#loginbtn').style.display = 'block';
