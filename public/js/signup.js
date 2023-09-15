264b1f56c54619984afa5a18cfef585b29fd5472
dldPwls03 <lyj3494@naver.com>
2023-09-15 21:15:24 +0900

    페스워드 토글

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,63 +1,94 @@
-const createBtn = document.getElementById('createBtn');
+document.addEventListener('DOMContentLoaded', function () {
+  const createBtn = document.getElementById('createBtn');
+  const emailInput = document.getElementById('userEmail');
+  const passwordInput = document.getElementById('userPassword');
+  const confirmPasswordInput = document.getElementById('userConfirmPassword');
+  const togglePassword = document.getElementById('togglePassword');
+  const toggleConfirmPassword = document.getElementById('toggleConfirmPassword'); // 확인 비밀번호의 눈 모양 아이콘
+  const authEmailBtn = document.getElementById('authEmailBtn'); // 이메일 인증 버튼
 
-createBtn.addEventListener('click', function () {
-  const email = document.getElementById('userEmail').value;
-  const authCode = document.getElementById('authCode').value;
-  const password = document.getElementById('userPassword').value;
-  const confirm = document.getElementById('userConfirmPassword').value;
-  const nickname = document.getElementById('userNickname').value;
+  // 패스워드 보이기/숨기기 토글 함수
+  function togglePasswordVisibility(passwordField) {
+    if (passwordField.type === 'password') {
+      passwordField.type = 'text';
+    } else {
+      passwordField.type = 'password';
+    }
+  }
 
-  const formData = {
-    email: email,
-    password: password,
-    confirm: confirm,
-    nickname: nickname,
-    authCode: authCode,
-  };
-  fetch('/signup', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(formData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.status === 201) {
-        alert(data.message);
-        window.location.href = '/login.html';
-      } else {
-        alert(data.message);
-      }
-    })
-    .catch((error) => {
-      console.error('회원가입 실패:', error);
-      alert(error.message);
-    });
-});
+  createBtn.addEventListener('click', function () {
+    const email = emailInput.value;
+    const authCode = document.getElementById('authCode').value;
+    const password = passwordInput.value;
+    const confirm = confirmPasswordInput.value;
+    const nickname = document.getElementById('userNickname').value;
 
-function authMailSending() {
-  const email = document.getElementById('userEmail').value;
-  if (email) {
-    fetch('/users/authemail', {
+    const formData = {
+      email: email,
+      password: password,
+      confirm: confirm,
+      nickname: nickname,
+      authCode: authCode,
+    };
+
+    fetch('/signup', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ email: email }),
+      body: JSON.stringify(formData),
     })
       .then((response) => response.json())
       .then((data) => {
-        if (data.status === 200) {
+        if (data.status === 201) {
           alert(data.message);
+          window.location.href = '/login.html';
         } else {
           alert(data.message);
         }
       })
       .catch((error) => {
-        console.error('Error:', error);
+        console.error('회원가입 실패:', error);
+        alert(error.message);
       });
-  } else {
-    alert('이메일을 입력해주세요.');
+  });
+
+  function authMailSending() {
+    const email = emailInput.value;
+    if (email) {
+      fetch('/users/authemail', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: email }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.status === 200) {
+            alert(data.message);
+          } else {
+            alert(data.message);
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    } else {
+        alert('이메일을 입력해주세요.');
+    }
   }
-}
+
+  // 패스워드 눈 모양 아이콘 클릭 이벤트
+  togglePassword.addEventListener('click', function () {
+    togglePasswordVisibility(passwordInput);
+  });
+
+  // 확인 비밀번호 눈 모양 아이콘 클릭 이벤트
+  toggleConfirmPassword.addEventListener('click', function () {
+    togglePasswordVisibility(confirmPasswordInput);
+  });
+
+  // 이메일 인증 버튼 클릭 이벤트
+  authEmailBtn.addEventListener('click', authMailSending);
+});
